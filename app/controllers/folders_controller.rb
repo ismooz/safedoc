5c4c615b37eef28bfafcdd1f05f4aab0ac91f91b
Ivan Perroud <ivan@perroud.me>
2021-03-15 12:07:29 +0100

    folder list view and css

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,2 +1,11 @@
 class FoldersController < ApplicationController
+  def index
+    @folders = policy_scope(Folder)
+  end
+
+  def show
+    @folder = Folder.find(params[:id])
+    authorize @folder
+  end
+  
 end
