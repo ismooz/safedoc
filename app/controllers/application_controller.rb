3fcd83ec00655cebffc9cd7580a7b6625d2895e0
ismael gashi <i.gashi@pm.me>
2021-03-16 21:44:54 +0100

    Add profile image to user and some fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   include Pundit
 
   # Pundit: white-list approach.
@@ -13,6 +16,12 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthdate, :address, :profile_image])
+  end
 
   private
 
