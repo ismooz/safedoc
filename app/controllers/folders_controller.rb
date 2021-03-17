4827a93703c3bc45a019262f72c7b3ab1319f25c
Ivan Perroud <ivan@perroud.me>
2021-03-17 16:50:32 +0100

    breadcrumb

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -6,11 +6,37 @@ class FoldersController < ApplicationController
   end
 
   def show
+    # folders to show
     @folders = Folder.where(parent_id: params[:id])
     authorize @folders
+
+    # parent folder
+    folder = Folder.find(params[:id])
+
+    # breadcrumb
+    breadcrumb = []
+    @breadcrumb = generate_breadcrumb(folder, breadcrumb)
+    
     # files
     @documents = Document.where(folder_id: params[:id])
     authorize @documents
   end
   
+  private
+
+  # we give a folder object as input. the method stops when Folder object as a nil parent_id
+  # in this case we are at the root folder. Recursive function :
+  def generate_breadcrumb(folder, arr = [])
+    if folder.parent_id.nil?
+      arr.unshift(folder)
+    else
+      # we add current id in first position
+      arr.unshift(folder)
+      # we get the parent folder
+      parent_folder = Folder.find(folder.parent_id)
+      # we call the function with parent folder
+      generate_breadcrumb(parent_folder, arr)
+    end
+  end
+
 end
