739f1778b50a80293a2633c6e461436bba831d3d
Antoine Robert <antoinero@gmail.com>
2021-03-25 00:56:44 +0100

    home page in progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,31 +4,9 @@ class PagesController < ApplicationController
   def home
     @document = Document.all.take(3)
 
-    docu_ddl = @document.sort_by { |doc| [doc.deadline]}
+    @docu_ddl = @document.sort_by { |doc| [doc.deadline]}
 
-    @document_per_deadline = docu_ddl.take(3)
-
-
-
-    # @ddl =[]
-
-    # @ddl << @deadlines
-
-    # @documentByDeadline = Document.deadline
-
-
-   # @document = Document.find(params[:id])
-   #  authorize @document
-
-
-
-    # doc infos
-     # @deadline = @document.deadline
-    # @updated_at = @document.updated_at.strftime("Last updated: %d %B %Y")
-    # @name = @document.name
-    # @thumb_key = @document.photos.first.key
-    # @pages = @document.photos.size
-    # @download_key = @document.photos.last.key
+    @docu_ddl = @docu_ddl.take(3)
 
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,9 @@ class User < ApplicationRecord
 
   # validations
   validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :birthdate, presence: true
-  validates :address, presence: true
+  # validates :last_name, presence: true
+  # validates :birthdate, presence: true
+  # validates :address, presence: true
   # validates :profile_image, presence: true
 
 end
