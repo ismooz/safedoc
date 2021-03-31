868e60059dabb97dbba70fd00d63477d82c43f2a
Ivan Perroud <ivan@perroud.me>
2021-03-31 17:10:27 +0200

    new documents and seed finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,19 @@
 require 'open-uri'
 require 'nokogiri'
 
-puts "Cleaning databases..."
-
 Type.destroy_all
-puts "destroy type"
 Document.destroy_all
-puts "destroy Document"
 Folder.destroy_all
-puts "destroy folder"
 User.destroy_all
-puts "destroy user"
 
-
-puts "Creating users..."
+puts "Database cleaned!"
 
 user_1 = User.create(email: "ismael@email.com",  password: "123456", first_name: "FFF", last_name: "LLL", birthdate: "2022-02-15", address: "bbbbbbbbb")
 user_2 = User.create(email: "ivan@email.com",  password: "123456", first_name: "Ivan", last_name: "Perroud", birthdate: "2022-02-15", address: "Chemin Ritter, Fribourg")
 user_3 = User.create(email: "antoine@email.com",  password: "123456", first_name: "FFF", last_name: "LLL", birthdate: "2022-02-15", address: "bbbbbbbbb")
+user_ids = User.all.map { |user| user.id }
 
-puts "Creating folders..."
+puts "users finished!"
 
 folder_1 = Folder.create(name: "Privé")
 Folder.create(name: "Administration", folder_id: folder_1.id)
@@ -34,9 +28,9 @@ Folder.create(name: "Postulations", folder_id: folder_2.id)
 
 folder_3 = Folder.create(name: "Autres")
 
-puts "Folders finished!"
+folder_ids = Folder.all.map { |folder| folder.id }
 
-puts "Creating types..."
+puts "folders finished!"
 
 types = [{ name: "Contrat"},
          { name: "Facture"},
@@ -48,11 +42,9 @@ types = [{ name: "Contrat"},
 types.each do |type|
   Type.create!(type)
 end
+
 puts "Types finished!"
 
-puts "Creating documents..."
-
-# path of images (relative)
 images_path = File.expand_path(".", Dir.pwd) + "/app/assets/images/doc_samples"
 puts images_path
 
@@ -60,53 +52,18 @@ date_1 = "2022/02/15"
 date_2 = "2023/07/11"
 date_3 = "2024/11/22"
 
-deadline_dates = [date_1, date_2, date_3]
+dates = [date_1, date_2, date_3]
 
 Dir.glob(images_path + "/*").each do |f|
-  filename_wo_extension = File.basename(f, ".pdf")
-  puts filename_wo_extension
+  filename_wo_extension = File.basename(f, ".jpg")
   filename = File.basename(f)
-  puts filename
   filepath = File.path(f)
-  puts filepath
   file = File.open(filepath)
-  puts file
 
-  deadline_date = deadline_dates.shuffle.first
-
-puts "test 1"
-  document = Document.new(name: "#{filename_wo_extension}", deadline: deadline_date, reminder: "2022/02/05", user_id: User.first.id, folder_id: Folder.first.id)
-puts "test 2"
-  # definition, variable
-  first = Hash.new
-  first[:key] = "1"
-  first[:url] = "https://res.cloudinary.com/ismooz/image/upload/v1615642798/bp4jixnmmbdbwzypmh29.jpg"
-  first[:extension] = "image/jpg"
-  current = first
-
-
-
-  # insertion, fix
-  file = URI.open(current[:url])
-  document.photos.attach(io: file, filename: "#{filename_wo_extension}_page#{current[:key]}", content_type: current[:extension])
+  document = Document.new(name: "#{filename_wo_extension}", deadline: dates.sample, reminder: "2022/02/05", user_id: user_ids.sample, folder_id: folder_ids.sample)
+  document.photos.attach(io: file, filename: "#{filename_wo_extension}", content_type: "image/jpg")
   document.save!
-
-  # definition, variable
-  second = Hash.new
-  second[:key] = "2"
-  second[:url] = "https://res.cloudinary.com/ismooz/image/upload/v1616085330/pw05d3nfn4is22ia0flbjzz1qyd4.png"
-  second[:extension] = "image/png"
-  current = second
-
-  # insertion, fix
-  file = URI.open(current[:url])
-  document.photos.attach(io: file, filename: "#{filename_wo_extension}_page#{current[:key]}", content_type: current[:extension])
-  document.save!
-
-  Cloudinary::Uploader.upload("#{images_path}/#{filename}",
-  :folder => "pdf_samples", :public_id => "#{filename_wo_extension}", :overwrite => true, :resource_type => "auto")
-
-  puts "Created #{document.id}"
+  puts "Created document id #{document.id}"
 end
 
-puts "Documents finished!"
+puts "seed finished!"
