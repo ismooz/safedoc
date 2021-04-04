4781f7ef70a20ebe71d827d3e31dd6acc47878ad
Ivan Perroud <ivan@perroud.me>
2021-04-04 16:45:23 +0200

    sorting asc

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :pricing]
 
   def home
-    @documents_sorted_creation = Document.all.order(created_at: :desc).take(3)
-    @documents_sorted_deadline = Document.all.order(deadline: :desc).take(3)
+    @documents_sorted_creation = Document.all.order(created_at: :asc).take(3)
+    @documents_sorted_deadline = Document.all.order(deadline: :asc).take(3)
   end
 
   def pricing
